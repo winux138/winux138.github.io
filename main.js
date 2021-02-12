6483a9f6df1d3b852f28f58f35140e580da9d147
Ju <jthomas@enssat.fr>
2021-02-12 10:09:52 +0100

    10 more

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ class Rond {
     }
 
     draw() {
+        ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
         ctx.fillStyle = this.color;
         ctx.fill();
@@ -20,6 +21,13 @@ var IntervId;
 
 var rond1 = new Rond(-1, -1, 10, "red");
 
+var rondAr = [];
+var intervIdAr = [];
+var workerAr = [];
+for (var i = 0; i < 10; ++i){
+    rondAr.push(new Rond(-1, -1, 20 * Math.random(), "#" + ((1<<24)*Math.random() | 0).toString(16)));
+}
+
 if (window.Worker) {
     monWorker = new Worker('worker.js');
 
@@ -29,17 +37,35 @@ if (window.Worker) {
         rond1.x = e.data[0];
         rond1.y = e.data[1];
         rond1.draw();
+
     }
 
-    IntervId = setInterval(drawCircle, 500 + 1000 * Math.random());
+    IntervId = setInterval(drawCircle(IntervId), 500 + 1000 * Math.random());
 
+
+    draw10more();
 
 } else {
     console.log("[ERREUR] pas possible d'utiliser des worker");
 }
 
-function drawCircle(rond) {
+function drawCircle(IntervId) {
     clearInterval(IntervId);
     monWorker.postMessage([canvas.width, canvas.height]);
     console.log('Message envoyé au worker');
+}
+
+function draw10more() {
+    for (let i = 0; i < 10; ++i){
+        workerAr.push(new Worker('worker.js'));
+        workerAr[i].onmessage = function (e) {
+            console.log('Message received from worker ' + i);
+            rondAr[i].x = e.data[0];
+            rondAr[i].y = e.data[1];
+            rondAr[i].draw();
+        }
+    
+        intervIdAr[i] = setInterval(drawCircle(intervIdAr[i]), 1500 + 1000 * Math.random());
+    }
+
 }
